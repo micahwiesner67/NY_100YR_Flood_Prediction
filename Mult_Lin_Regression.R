70ea7efdbde2e0e71474ea8379bacb0d35783331
Micah Wiesner <33739832+micahwiesner67@users.noreply.github.com>
2020-06-23 21:01:55 -0400

    Update Mult_Lin_Regression.R

--- Mult_Lin_Regression.R
+++ Mult_Lin_Regression.R
@@ -1,6 +1,12 @@
 #Micah Wiesner
 #December 2018
 
+#Packages
+#### start ####
+install.packages('glmnet')
+library(glmnet)
+#### end ####
+
 peakflow_df <- read.csv("Peak_Flow.csv", sep = ",", header = T, na.strings = c("0", "NA"))
 
 #This dataframe is 194x105 and contains information from NY state creeks and rivers
@@ -24,15 +30,18 @@ test <- peakflow_df[-train_ind, ]
 #The variable we want to predict is Q100. Q100 is the height of the river/stream in an extreme
 #100 year flood
 
-numeric_df <- data.frame(train[6:105])
-target <- unlist(train[5])
+train_X <- data.frame(train[6:105])
+train_Y <- unlist(train[5])
+
+test_X <- data.frame(test[6:105])
+test_Y <- unlist(test[5])
 
 store_cor <- vector(mode = 'numeric', length(numeric_df))
 name_var <- vector(mode = 'character', length(numeric_df))
 
-for (i in 1:length(numeric_df)){
-  store_cor[i] <- cor(target, numeric_df[i])^2
-  name_var[i] <- colnames(numeric_df)[i]
+for (i in 1:length(train_X)){
+  store_cor[i] <- cor(target, train_X[i])^2
+  name_var[i] <- colnames(train_X)[i]
   if (i == 100){
     cor_df <- data.frame(name_var, store_cor)
     cor_df <- cor_df[order(cor_df$store_cor, decreasing = T),]
@@ -48,17 +57,12 @@ explanatory_variables <- head(cor_df, n = 10)
 #We will utilize the the drainage area, max elevation of the basin, and a few other variables in our multiple linear regression
 
 #Multiple Linear Regression with the 10 most predictive 
-keep_cols <- explanatory_variables$name_var
-  
-explan_vars_df <- data.frame(numeric_df[ , keep_cols])
-final_df <- data.frame(target, explan_vars_df)
-
-fit <- lm(target ~ explan_vars_df$DRAIN_SQKM + explan_vars_df$ELEV_MAX_M_BASIN +
-     explan_vars_df$ECO3_BAS_DOM + explan_vars_df$PET + 
-     explan_vars_df$BFI_AVE + explan_vars_df$FST32F_BASIN + 
-     explan_vars_df$ELEV_MEAN_M_BASIN + explan_vars_df$ELEV_MEDIAN_M_BASIN +
-     explan_vars_df$LST32F_BASIN + explan_vars_df$FLOWYRS_1900_2009
-     )
+fit <- lm(Q100 ~ DRAIN_SQKM + ELEV_MAX_M_BASIN +
+            ECO3_BAS_DOM + PET + 
+            BFI_AVE + FST32F_BASIN + 
+            ELEV_MEAN_M_BASIN + ELEV_MEDIAN_M_BASIN +
+            LST32F_BASIN + FLOWYRS_1900_2009,
+          data = train)
 
 #Now create a function to extract regression coefficents to be able to use with 
 #testing dataset
@@ -77,21 +81,28 @@ regEq <- function(lmObj, dig) {
   )
 }
 
-explan_vars_df <- test 
+eq <- regEq(fit, 1)
 
-model_results <- eval(formula(regEq(fit,1)))
-actual_results <- test$Q100
+#I was having trouble with the predict function and so I am just hard-coding the below equation
+model_Y <- 286524.2 + 9.3*test_X$DRAIN_SQKM - 2.1*test_X$ELEV_MAX_M_BASIN - 
+    290.1*test_X$ECO3_BAS_DOM - 30.4*test_X$PET - 448*test_X$BFI_AVE - 
+    362.3*test_X$FST32F_BASIN + 192.7*test_X$ELEV_MEAN_M_BASIN - 
+    165.6*test_X$ELEV_MEDIAN_M_BASIN - 1001.5*test_X$LST32F_BASIN + 116.3*test_X$FLOWYRS_1900_2009
 
 xx <- yy <- seq(0,200000) 
-summary(fit)
+cor_MLR <- round(cor(model_Y, test_Y)^2,2)
 
 plot.new()
 par(mfrow = c(1,2))
-plot(model_results, actual_results, main = 'Model Comparison', 
+plot(model_Y, test_Y, main = 'MLR Comparison', 
      xlab = 'Model', ylab = 'Actual (Testing Set)',
-     text(100000, 175000, 'R2 = 0.597'))
+     text(100000, 175000, paste('R2 =', cor_MLR)))
 lines(xx, yy, lty = 2)
 
-my_residuals <- model_results - actual_results
+my_residuals <- model_Y - test_Y
+RMS_MLR <- round(sum(abs(my_residuals)))
+
 hist(my_residuals, main = 'Testing for Normalcy',
      xlab = 'Residuals', col = 'lavender')
+
+
